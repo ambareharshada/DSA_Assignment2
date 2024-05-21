9edfadd6442a1cd92fe06049782deaf108bef130
ambareharshada <ambareharshada88@gmail.com>
2024-05-21 16:15:02 +0530

    Print star pattern program

---
+++ StarPattern.js
@@ -0,0 +1,11 @@
+var rows = 5;
+var pattern = "";
+
+function printStarPattern(rows) {
+  for (var i = 0; i < rows; i++) {
+    pattern = pattern + " *";
+    console.log(pattern);
+  }
+}
+ printStarPattern(rows);
+
